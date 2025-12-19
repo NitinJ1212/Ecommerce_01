81c6a84d6af451938fad8b4f5cc74b0e5eececa7
Nitin Jangra <fsdnitin2023@gmail.com>
2025-12-19 14:44:06 +0530

    udpate

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
 
 import { protect } from "../middleware/authMiddleware.js";
-import { createDummyProducts, getAllProducts, getProductById } from "../controllers/productController.js";
+import { createDummyProducts, getAllProducts, getProductById, filterProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
 // Public routes
-router.post("/addproducts", createDummyProducts);
+router.post("/addproducts", protect, createDummyProducts);
 
-router.get("/", getAllProducts);
+router.get("/", protect, getAllProducts);
 
-router.post("/:id", getProductById);
+router.post("/:id", protect, getProductById);
+
+router.post("/filter/products", protect, filterProducts);
+// router.post("/filter", protect, filterProducts);
 
 export default router;
